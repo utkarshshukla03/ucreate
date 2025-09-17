09e3ccef7dc8e8adcfa15b616e5d7513706c61b9
Utkarsh Shukla <utkarshshukla372@gmail.com>
2025-09-18 01:30:15 +0530

    Fixed config

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,2 +1,2 @@
-// export const BACKEND_URL = "https://ucreate-backend.onrender.com"
-export const BACKEND_URL = "http://localhost:3001"; // Local development
+export const BACKEND_URL = "https://ucreate-backend.onrender.com"
+// export const BACKEND_URL = "http://localhost:3001"; // Local development
